f237c8808f69cd724511d92b40d93e1050155045
EAjooen <161267520+EAjooen@users.noreply.github.com>
2024-04-29 12:27:32 +0800

    Add files via upload

---
+++ Detection.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import streamlit as st
+import pandas as pd
+import cv2
+from PIL import Image, ImageEnhance
+import numpy as np
+import os
+#import tensorflow as tf
+#import tensorflow_hub as hub
+import time ,sys
+from streamlit_embedcode import github_gist
+import urllib.request
+import urllib
+import moviepy.editor as moviepy
+import cv2
+import numpy as np
+import time
+import sys
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
+
+
+st.write("check out this [link](https://share.streamlit.io/detection.py)")
+st.markdown(link,unsafe_allow_html=True)
+
+
+def object_detection_image():
+    st.title('Cat and Dog Detection for Images')
+    st.subheader("""
+    
+    """)
+    d = st.markdown("""
+    Please scroll down to see the processed image."""
+    )
+    file = st.file_uploader('Upload Image', type = ['jpg','png','jpeg'])
+    if file!= None:
+        img1 = Image.open(file)
+        img2 = np.array(img1)
+
+        st.image(img1, caption = "Uploaded Image")
+        my_bar = st.progress(0)
+
+        #classNames = []
+       
+        # Disable scientific notation for clarity
+        np.set_printoptions(suppress=True)
+
+        # Load the model
+        model = load_model("keras_Model.h5", compile=False)
+
+        # Load the labels
+        class_names = open("labels.txt", "r").readlines()
+
+        # Create the array of the right shape to feed into the keras model
+        # The 'length' or number of images you can put into the array is
+        # determined by the first position in the shape tuple, in this case 1
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+        # Replace this with the path to your image
+        image = img1.convert("RGB")
+
+        # resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+        # turn the image into a numpy array
+        image_array = np.asarray(image)
+
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+        # Load the image into the array
+        data[0] = normalized_image_array
+
+        # Predicts the model
+        prediction = model.predict(data)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
+
+        # Print prediction and confidence score
+        print("Class:", class_name[2:], end="")
+       
+
+           
+       
+    
+        st.image(img2, caption=class_name[2:])
+        
+        cv2.waitKey(0)
+        
+        cv2.destroyAllWindows()
+        my_bar.progress(100)
+
+
+
+def main():
+    new_title = '<p style="font-size: 42px;">Welcome to Cat and Dog Detection App!</p>'
+    read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
+
+    read_me = st.markdown("""
+    This project was built using Streamlit
+    to demonstrate Cat and Dig detection in images."""
+    )
+    choice  = st.sidebar.selectbox("MODE",("About","Image"))
+    #["Show Instruction","Landmark identification","Show the #source code", "About"]
+    
+    if choice == "Image":
+        #st.subheader("Object Detection")
+        read_me_0.empty()
+        read_me.empty()
+        #st.title('Object Detection')
+        object_detection_image()
+   
+        
+
+    elif choice == "About":
+        print()
+        
+
+if __name__ == '__main__':
+		main()	
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
