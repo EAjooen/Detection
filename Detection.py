ce50c73ac2d6f13e870f63595983588cb60d99dd
EAjooen <161267520+EAjooen@users.noreply.github.com>
2024-04-29 20:58:56 +0800

    Update Detection.py

--- Detection.py
+++ Detection.py
@@ -47,7 +47,7 @@ def object_detection_image():
         np.set_printoptions(suppress=True)
 
         # Load the model
-        model = load_model("keras_model.h5", compile=False)
+        model = load_model("keras_model.h5", compile=True)
 
         # Load the labels
         class_names = open("labels.txt", "r").readlines()
